aed3c83186392a654070cdb69525e9f575a0558c
AltoHearts <72461104+AltoHearts@users.noreply.github.com>
2021-03-17 19:21:12 -0700

    test

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 var express = require('express'); 
 
- 
- 
-
 var app = express(); 
-
 const PORT = 3000; 
 
- 
- 
-
 app.set('view engine','ejs');//sent views to EJS 
-
- 
- 
 
 //this is how a route is built in Express 
 
@@ -21,12 +11,9 @@ app.get('/', (req,res)=>{
 
   var title = "Our Home Page"; 
 
-  res.render('index',{title:title}); 
+  res.render('pages/index',{title:title}); 
 
 }); 
-
- 
- 
 
 app.listen(PORT,()=>{ 
 
