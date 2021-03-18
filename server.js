899aeb9cb3f5cf281550873ce5657bb15e1d9743
AltoHearts <72461104+AltoHearts@users.noreply.github.com>
2021-03-17 19:56:41 -0700

    adds user data to server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express'); 
+var data = require('./data/test.json'); 
 
 var app = express(); 
 const PORT = 3000; 
@@ -18,5 +19,5 @@ app.get('/', (req,res)=>{
 app.listen(PORT,()=>{ 
 
   console.log(`App is running on port ${PORT}`); 
-
+  console.log(data);
 }); 
